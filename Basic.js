c4cea77f1999cf9094c5d5c42279ae88a811b24e
aman <ay845790@gmail.com>
2026-02-25 12:43:23 +0530

    js question

---
+++ Basic.js
@@ -0,0 +1,26 @@
+// Reverse a String
+function reverseString(str) {
+  return str.split("").reverse().join("");
+}
+
+console.log(reverseString("aman"));
+// Output: nama
+
+// Check Palindrome 2 //
+
+function isPalindrome(str) {
+  const reversed = str.split("").reverse().join("");
+  return str === reversed;
+}
+
+console.log(isPalindrome("madam")); // true
+console.log(isPalindrome("hello")); // false
+
+// Find Largest Number in Array 3 //
+
+function findLargest(arr) {
+  return Math.max(...arr);
+}
+
+console.log(findLargest([10, 5, 25, 8]));
+// Output: 25
