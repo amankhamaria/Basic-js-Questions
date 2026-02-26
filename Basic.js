6eac6e575263372a066c593c3e17e7934cce5864
aman <ay845790@gmail.com>
2026-02-26 10:04:15 +0530

    js if else question

--- Basic.js
+++ Basic.js
@@ -1,26 +1,43 @@
 // Reverse a String
-function reverseString(str) {
-  return str.split("").reverse().join("");
-}
+// function reverseString(str) {
+//   return str.split("").reverse().join("");
+// }
 
-console.log(reverseString("aman"));
+// console.log(reverseString("aman"));
 // Output: nama
 
 // Check Palindrome 2 //
 
-function isPalindrome(str) {
-  const reversed = str.split("").reverse().join("");
-  return str === reversed;
-}
+// function isPalindrome(str) {
+//   const reversed = str.split("").reverse().join("");
+//   return str === reversed;
+// }
 
-console.log(isPalindrome("madam")); // true
-console.log(isPalindrome("hello")); // false
+// console.log(isPalindrome("madam")); // true
+// console.log(isPalindrome("hello")); // false
 
 // Find Largest Number in Array 3 //
 
-function findLargest(arr) {
-  return Math.max(...arr);
-}
+// function findLargest(arr) {
+//   return Math.max(...arr);
+// }
 
-console.log(findLargest([10, 5, 25, 8]));
+// console.log(findLargest([10, 5, 25, 8]));
 // Output: 25
+
+// Variable kaise declare karte hain? ///////////////
+// var name = "Aman";
+// let age = 25;
+// const country = "India";
+
+// console.log(name, age, country);
+
+// If-Else Example /////////
+
+// let age = 18;
+
+// if (age >= 18) {
+//   console.log("Adult");
+// } else {
+//   console.log("Minor");
+// }
